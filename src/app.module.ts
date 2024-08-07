0f263e6725e9047917348a07e19ec46f9fb2aae9
Prashant Kumar <prashantnawa123@gmail.com>
2024-08-07 15:36:29 +0530

    calculator api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CalcModule } from './calc/calc.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [CalcModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*'); // Apply to all routes
+  }
+}
--- src/calc/calc.service.ts
+++ src/calc/calc.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { CalcDto } from './calc.dto';
 
 @Injectable()
 export class CalcService {
-  calculateExpression(calcBody: CalcDto) {
-    return 0;
+  calculateExpression(calcDto: CalcDto) {
+    try {
+      let expression = calcDto.expression.replace(/\s+/g, '');
+
+      if (!this.isValidExpression(expression)) {
+        throw new BadRequestException('Invalid expression provided');
+      }
+
+      const result = this.evaluate(expression);
+
+      return { statusCode: 200, result: result };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  private isValidExpression(expression: string): boolean {
+    const validRegex = /^(\d+([+\-*/]\d+)+)+$/;
+    return validRegex.test(expression);
+  }
+
+  private evaluate(expression: string): number {
+    const numbers = expression.split(/[\+\-\*\/]/).map(num => parseFloat(num));
+    const operators = expression.split(/\d+/).filter(op => op);
+
+    if (numbers.length - 1 !== operators.length) {
+      throw new BadRequestException('Invalid expression format');
+    }
+
+    let result = numbers[0];
+    for (let i = 0; i < operators.length; i++) {
+      switch (operators[i]) {
+        case '+':
+          result += numbers[i + 1];
+          break;
+        case '-':
+          result -= numbers[i + 1];
+          break;
+        case '*':
+          result *= numbers[i + 1];
+          break;
+        case '/':
+          if (numbers[i + 1] === 0) throw new BadRequestException('Division by zero');
+          result /= numbers[i + 1];
+          break;
+      }
+    }
+    return result;
   }
 }
